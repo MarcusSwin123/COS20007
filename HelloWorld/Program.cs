33510c534384dc40e2d8bc81a1d3ae58c13ffd83
MARCUS ARENA <103064928@student.swin.edu.au>
2023-03-20 12:41:10 +1100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Message responses;
+            Message[] _responses = { new Message("Welcome Back!"),
+            new Message("What a loveely name!"),
+            new Message("Great Name!"),
+            new Message("Oh hi!"),
+            new Message("That is a silly name") };
+
+            string name;
+
+            Message myMessage;
+            myMessage = new Message("Hello World - from Message Object");
+            myMessage.Print();
+
+            while(true)
+
+            {
+                Console.WriteLine("Enter name:");
+                name = Console.ReadLine();
+                if (name == "Mark")
+                {
+                    _responses[0].Print();
+                }
+                else if (name == "Fred")
+                {
+                    _responses[1].Print();
+                }
+                else if (name == "Wilma")
+                {
+                    _responses[2].Print();
+                }
+                else if (name =="Alice")
+                {
+                    _responses[3].Print();
+                }
+                else
+                {
+                    _responses[4].Print();
+                }
+            }
+                                   
+        }
+    }
+}
