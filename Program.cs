21aa539ad99ca3b1130f5c48e6cf87fb00e248f3
MarcusSwin123 <128335702+MarcusSwin123@users.noreply.github.com>
2023-03-24 12:53:25 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using System;
+using SplashKitSDK;
+
+namespace ShapeDrawer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Shape myShape = new Shape();
+
+            Window window = new Window("Shape Drawer", 800, 600);
+            do
+            {
+                SplashKit.ProcessEvents();
+                SplashKit.ClearScreen();
+
+                if (SplashKit.MouseClicked(MouseButton.LeftButton))
+                {
+                    myShape.x = (float)SplashKit.MouseX();
+                    myShape.y = (float)SplashKit.MouseY();
+                }
+
+                if (myShape.IsAt(SplashKit.MousePosition()) && SplashKit.KeyTyped(KeyCode.SpaceKey))
+                {
+                    myShape.color = Color.RandomRGB(255);
+                }
+
+                myShape.Draw();
+                SplashKit.RefreshScreen();
+            } while (!window.CloseRequested);
+        }
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,61 @@
+﻿using SplashKitSDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeDrawer
+{
+    public class Shape
+    {
+        private Color _color;
+        private float _width, _height;
+        private float _x, _y;
+        public Shape()
+        {
+            _color = Color.Green;
+            _width = _height = 100;
+            _x = _y = 0;
+        }
+        public void Draw()
+        {
+            SplashKit.FillRectangle(_color, _x, _y, _width, _height);
+        }
+
+        public Color color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+        public float Width 
+        { 
+            get { return _width; }
+            set { _width = value; }
+        }
+        public float Height 
+        { 
+            get { return _height;} 
+            set { _height = value; }
+        }  
+        public float x 
+        { 
+            get { return _x; } 
+            set { _x = value; }
+        }
+        public float y 
+        { 
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public bool IsAt(Point2D pt)
+        {
+            if (_x < pt.X && pt.X > _width && _x < pt.Y && pt.Y > _height);
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
